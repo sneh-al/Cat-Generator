21ee89074924d0323cf872521d448f688a85a63d
lycan <snehal1380@gmail.com>
2023-06-26 14:54:44 +0530

    intial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className='grid place-content-center bg-red h-screen  bg-slate-900'>
+      <h1 className='font-bold text-gray-300 text-2xl underline'>Intial</h1>
+    </div>
+  );
+}
+
+export default App;
